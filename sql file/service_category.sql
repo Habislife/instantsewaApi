c8b8faba610eb3791ad32b06ba61e953b99d804a
Bishal Devkota <devkotahancy@gmail.com>
2020-10-15 02:27:12 +0545

    Signed-off-by: Bishal Devkota <devkotahancy@gmail.com>

--- sql file/service_category.sql
+++ sql file/service_category.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2020 at 10:38 PM
+-- Generation Time: Oct 14, 2020 at 10:40 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -22,20 +22,36 @@ SET time_zone = "+00:00";
 --
 
 --
--- Dumping data for table `service_provider-service`
+-- Dumping data for table `service_category`
 --
 
-INSERT INTO `service_provider-service` (`service_provider_id`, `service_id`) VALUES
-(11, 11),
-(10, 5),
-(12, 3),
-(3, 3),
-(4, 23),
-(5, 19),
-(6, 16),
-(7, 17),
-(8, 15),
-(9, 25);
+INSERT INTO `service_category` (`category_id`, `service_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(1, 7),
+(1, 8),
+(1, 9),
+(1, 10),
+(1, 11),
+(1, 12),
+(1, 13),
+(1, 14),
+(1, 15),
+(1, 17),
+(1, 16),
+(1, 18),
+(1, 19),
+(1, 20),
+(1, 21),
+(1, 22),
+(1, 23),
+(1, 24),
+(1, 25),
+(1, 26);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
