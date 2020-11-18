93dce019ac4b62b0460af2b5b666bb207fc5110f
Habis <devkothancy@gmail.com>
2020-11-18 16:40:23 +0545

    123

--- sql file/service_provider-service.sql
+++ sql file/service_provider-service.sql
@@ -1,31 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.0.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 18, 2020 at 11:50 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: instantsewa
---
 
---
--- Dumping data for table service_provider-service
---
-
-INSERT INTO service_provider-service (service_provider_id, subcategories_id) VALUES
+INSERT INTO `service_provider-service` VALUES
 (3, 1),
 (4, 2),
 (5, 3),
@@ -39,7 +21,6 @@ INSERT INTO service_provider-service (service_provider_id, subcategories_id) VAL
 (3, 2),
 (4, 1),
 (6, 1);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
